a391dc62fd41c182de313f275a94dea398e339fa
Andrei <htvgamer@gmail.com>
2019-12-23 17:21:40 +0100

    es 2/12/12

---
+++ ex2-17-12-2019/ex2-17-12-2019.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+string getAlbero(int n);
+main()
+{
+   int n;
+   cin >> n;
+   if(n<3){
+      cout << "Altezza non valida";
+   }else{
+     cout << getAlbero(n);
+   } 
+}
+
+string getAlbero(int n){
+   string albero;
+   int blanks = 0;
+   int stars = 1;
+   //calcolo blanks iniziali
+   for(size_t i = 2; i < n; i++){
+      blanks++;
+   }
+   //Salvo la base
+   string base;
+   for (size_t i = 0; i < blanks; i++){
+      base+="b";
+   }
+   base+="s";
+   //riempio albero
+   for (size_t i = 0; i < n-1; i++){
+      //riempio blanks
+      for (size_t e = 0; e < blanks; e++){
+         albero+="b";
+      }
+      //riempio stars
+      for (size_t e = 0; e < stars; e++){
+         albero+="s";
+      }
+      //decremento e incremento i blanks e gli star
+      blanks--;
+      stars = stars + 2;
+      albero+="\n";
+   }
+   //aggiungo la bse
+   albero+=base;
+   //return della funzione
+   return albero;
+}
